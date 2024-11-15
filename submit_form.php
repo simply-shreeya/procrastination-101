6240a16d5361824062b56e4cffea247278d18961
Srijan Baniyal <john.rambo.9901@gmail.com>
2024-11-15 22:29:35 +0530

    Added docs

--- submit_form.php
+++ submit_form.php
@@ -4,21 +4,14 @@ $name = $email = $message = "";
 $name = $_POST["name"];
 $email = $_POST["email"];
 $message = $_POST["message"];
-
 $server_name = "localhost";
 $user_name = "root";
 $sqlpsw = "test123";
 $connection = mysqli_connect($server_name, $user_name, $sqlpsw, "forum_db");
-
 if ($connection) {
-    // Create database if it doesn't exist
     $db_query = "CREATE DATABASE IF NOT EXISTS forum_db";
     mysqli_query($connection, $db_query);
-
-    // Select the database
     mysqli_select_db($connection, "forum_db");
-
-    // Create table if it doesn't exist
     $table_query = "CREATE TABLE IF NOT EXISTS feedback (
         id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
         name VARCHAR(50) NOT NULL,
@@ -27,8 +20,6 @@ if ($connection) {
         reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
     )";
     mysqli_query($connection, $table_query);
-
-    // Insert data into the table
     $query = "INSERT INTO feedback (name, email, message) VALUES ('$name', '$email', '$message')";
     if (mysqli_query($connection, $query)) {
         echo "Successfully inserted data";
